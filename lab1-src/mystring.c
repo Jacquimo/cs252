76598b47d7d25a2f784b3bf3d8966e0cd7809e10
Gray Houston <ghousto@purdue.edu>
2015-01-14 13:07:15 -0500

    Added the source code for lab 1

---
+++ lab1-src/mystring.c
@@ -0,0 +1,41 @@
+#include <sys/types.h>
+
+/*
+ * Implement the following string procedures.
+ *
+ * Type "man strstr" to find what each of the functions should do.
+ *
+ * For example, mystrcpy should do the same as strcpy.
+ *
+ * IMPORTANT: DO NOT use predefined string functions.
+ */
+
+char *mystrcpy(char * s1, const char * s2)
+{
+  /* Complete procedure */
+}
+
+size_t mystrlen(const char *s)
+{
+  /* Complete procedure */
+}
+
+char *mystrdup(const char *s1)
+{
+  /* Complete procedure */
+}
+
+char *mystrcat(char * s1, const char * s2)
+{
+  /* Complete procedure */
+}
+
+char *mystrstr(char * s1, const char * s2)
+{
+  /* Complete procedure */
+}
+
+int mystrcmp(const char *s1, const char *s2) {
+  /* Complete procedure */
+}
+
