8376815849e4d4c9f05fa255059d666c1a16df46
Gray Houston <ghousto@purdue.edu>
2015-01-16 12:36:48 -0500

    Completed Queue.h file

--- lab1-src/Queue.h
+++ lab1-src/Queue.h
@@ -1,8 +1,12 @@
 #include "List.h"
 
 class Queue : List {
+	void enqueue(Node* node);
+	void dequeue(Node* node);
 
 public:
+	void enqueue(int val);
+	void dequeue(int val);
 	Queue();
 	~Queue();
 };
