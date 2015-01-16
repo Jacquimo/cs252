c2f17daa35151cdace3ee97e98742bd39be2a607
Gray Houston <ghousto@purdue.edu>
2015-01-16 18:25:32 -0500

    fixed add function

--- lab1-src/pitfalls.cpp
+++ lab1-src/pitfalls.cpp
@@ -60,10 +60,12 @@ like this:
 int x = 10, y = 20;
 int sum = add(&x, &y);*/
 int add(int *n1, int *n2) {
-  int sum = *n1 + *n2;
+  /*int sum = *n1 + *n2;
   delete n1;
   delete n2;
   return sum;
+  */
+  return *n1 + *n2;
 }
 
 /*The following function copies n chars
