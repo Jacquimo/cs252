a543929d2114d299096c4526816bba270ca5db5c
Gray Houston <ghousto@purdue.edu>
2015-01-16 18:00:20 -0500

    Implemented mysort functionality. Passes all test cases

--- lab1-src/mysort.cc
+++ lab1-src/mysort.cc
@@ -2,6 +2,10 @@
 #include <alloca.h>
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
+
+static void insertionSort(int, int, void*, void*, int, CompareFunction);
+static void swap(void*, void*, int);
 
 //
 // Sort an array of element of any type
@@ -21,6 +25,29 @@ void mysort( int n,                      // Number of elements
 	     int ascending,              // 0 -> descending; 1 -> ascending
 	     CompareFunction compFunc )  // Comparison function.
 {
-  // Add your code here. Use any sorting algorithm you want.
+	insertionSort(n, elementSize, array, array + (n-1)* elementSize, ascending, compFunc);
+}
+
+void insertionSort(int n, int elementSize, void* start, void* end, int ascending, 
+	CompareFunction comp) {
+	for (void* i = end - elementSize; i >= start; i -= elementSize) {
+		for (void* j = i; j < end; j += elementSize) {
+			int comparison = comp(j, j + elementSize);
+			comparison = ascending ? comparison : comparison * -1;
+
+			if (comparison > 0)
+				swap(j, j + elementSize, elementSize);
+			else
+				break;
+		}
+	}
+}
+
+void swap(void* first, void* second, int elementSize) {
+	void* temp = malloc(elementSize);
+	temp = memcpy(temp, first, elementSize);
+	memcpy(first, second, elementSize);
+	memcpy(second, temp, elementSize);
+	free(temp);
 }
 
