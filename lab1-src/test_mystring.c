ac853865638a6234033c1af334ebcadc98acad05
Gray Houston <ghousto@purdue.edu>
2015-01-22 20:55:10 -0500

    Added test cases for mystring functions

--- lab1-src/test_mystring.c
+++ lab1-src/test_mystring.c
@@ -20,37 +20,63 @@ main()
 
   /* Test mystrcpy */
   mystrcpy (s1, "Hello world");
+  assert(strlen(s1) == 11);
   assert(!strcmp (s1, "Hello world"));
   assert(strcmp (s1, "Hello worl"));
   assert(strcmp (s1, "Hello worldgggg"));
+  assert(strcmp(s1, ""));
+  assert(strcmp (s1, "ello world"));
+  assert(strcmp (s1, "ello worl"));
 
   mystrcpy (s1, "");
+  assert(strlen(s1) == 0);
   assert(!strcmp (mystrcpy (s1, ""), ""));
+  assert(strlen(s1) == 0);
   printf("mystrcmp: pass\n");
 
   /* Test mystrlen */
   assert (mystrlen("Hello world") == 11);
-  assert (mystrlen("Hello world") == 11);
-
-  assert (mystrlen("Hello world") == 11);
-  assert (mystrlen("Hello world") == 11);
+  assert (mystrlen("CS252 Systems Programming") == strlen("CS252 Systems Programming"));
+  assert (mystrlen("\b") == strlen("\b"));
+  assert (mystrlen("") == strlen(""));
   printf("mystrlen: pass\n");
  
   /* Test strdup */
   s2 = mystrdup("Hello world");
   assert( !strcmp(s2, "Hello world") );
+  assert(strlen(s2) == 11);
+  assert(strlen(s2) == mystrlen(s2));
+
   s2[0]='Y';
   assert( !strcmp(s2, "Yello world") );
+  assert(strlen(s2) == 11);
+  assert(strlen(s2) == mystrlen(s2));
+
+  s2[10] = '\0';
+  assert( !strcmp(s2, "Yello worl") );
+  assert(strlen(s2) == 10);
+  assert(strlen(s2) == mystrlen(s2));
+
+  free(s2);
+  s2 = mystrdup("");
+  assert(!strcmp(s2, ""));
+  assert(mystrcmp(s2, "") == strcmp(s2, ""));
+  assert(strlen(s2) == 0);
+  assert(mystrlen(s2) == strlen(s2));
+
   free(s2);
   printf("mystrdup: pass\n");
 
   // Test mystrcat
   mystrcpy(s1, "Purdue");
+  assert(mystrlen(s1) == strlen(s1));
   mystrcat(s1, " University");
-  assert( !strcmp(s1, "Purdue University")); 
+  assert(mystrlen(s1) == strlen(s1));
+  assert( !strcmp(s1, "Purdue University"));
 
   mystrcat(s1, "");
-  assert( !strcmp(s1, "Purdue University")); 
+  assert(mystrlen(s1) == strlen(s1));
+  assert( !strcmp(s1, "Purdue University"));
   printf("mystrcat: pass\n");
 
   // Test mystrstr
